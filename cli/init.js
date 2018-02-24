056fd6bd1e5f027c7deb1e0f4bfc34ffc55a0081
Matej Šircelj <sircelj.m@gmail.com>
2018-02-24 16:41:11 +0100

    fixed init

--- cli/init.js
+++ cli/init.js
@@ -1,17 +1,14 @@
 var path = require("path");
 var fs = require("fs");
 
-module.exports = function(vorpal, projectConfig){
+module.exports = function(vorpal, cwd){
     return vorpal
     	.command( "init [dir]", "Init Awly directory" )
-        // TODO options for tags and branches
     	.action( function( args, cb ) {
             if(!args.dir){
                 args.dir = cwd;
             }
             require("../lib/init")(args.dir);
-    		// invokes command code in module providing vorpal and arguments, supporting promise as result
-    		// Promise.resolve( ( this, args ) ).then( projectConfig.repl ? cb : null );
     	} )
         .on('error', function(err){
             console.log(err);
