d17f824f4d9c730ee86602a71afe8b1810df8eec
Matej Šircelj <sircelj.m@gmail.com>
2018-05-27 10:58:34 +0200

    component family tree overview

---
+++ web/src/hooks/inline-css-transformer.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const mime = require('mime');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = function transform(el, context) {
+    let inlineCSSExpression = el.getAttributeValue('inline-css');
+    /*
+    inlineCSSExpression is an AST node that represents the attribute value parsed as a path to CSS file.
+     */
+
+    if (!inlineCSSExpression) {
+        return;
+    }
+
+    let builder = context.builder;
+
+    el.removeAttribute('inline-css'); // This attribute is handled at compile time. We can just remove it now
+
+    if (inlineCSSExpression.type === 'Literal') {
+        /*
+        The path is a literal value that we can process at compile-time. Our goal is to set the style content
+        to the contents of the file under the defined path.
+
+        The AST for `"./marko-logo.png"` looks like the following:
+        {
+            type: 'Literal',
+            value: './marko-logo.png'
+        }
+        */
+        let cssPath = "";
+
+        if(path.isAbsolute(inlineCSSExpression.value)){
+            const appDir = path.resolve(__dirname, '../..');
+            cssPath = path.normalize(appDir + inlineCSSExpression.value);
+        }else{
+            const templateDir = context.dirname;
+            cssPath = path.resolve(templateDir, inlineCSSExpression.value);
+        }
+
+        let data = fs.readFileSync(cssPath);
+        var style = context.builder.htmlElement('style', [], [context.builder.text(context.builder.literal(data.toString()))]);
+
+        // el.setBody(builder.literal(data));
+        el.replaceWith(style);
+
+        // End result:
+        // <img width=150 height=125 src="data:image/png;base64,...">
+    } else {
+        /*
+        Otherwise, the attribute value is a dynamic JavaScript expression that can only be handled
+        at runtime.
+
+        Dont do anything for now in this case
+        */
+    }
+};
---
+++ web/src/hooks/inline-svg-transformer.js
@@ -0,0 +1,123 @@
+'use strict';
+
+const mime = require('mime');
+const fs = require('fs');
+const path = require('path');
+const SVGO = require('svgo');
+const deasync = require("deasync");
+
+function svgoOptimizeSync(svgo, content) {
+  let res;
+  svgo.optimize(content).then(result => res = result);
+  deasync.loopWhile(() => !res);
+  return res;
+}
+
+module.exports = function transform(el, context) {
+    let inlineSVGExpression = el.getAttributeValue('inline-svg');
+    let svgClasses = el.getAttributeValue('class') || {};
+
+    const svgo = new SVGO({
+        plugins: [{
+          cleanupAttrs: true,
+        }, {
+          removeDoctype: true,
+        },{
+          removeXMLProcInst: true,
+        },{
+          removeComments: true,
+        },{
+          removeMetadata: true,
+        },{
+          removeTitle: true,
+        },{
+          removeDesc: true,
+        },{
+          removeUselessDefs: true,
+        },{
+          removeEditorsNSData: true,
+        },{
+          removeEmptyAttrs: true,
+        },{
+          removeHiddenElems: true,
+        },{
+          removeEmptyText: true,
+        },{
+          removeEmptyContainers: true,
+        },{
+          removeViewBox: false,
+        },{
+          cleanUpEnableBackground: true,
+        },{
+          convertStyleToAttrs: true,
+        },{
+          convertColors: true,
+        },{
+          convertPathData: true,
+        },{
+          convertTransform: true,
+        },{
+          removeUnknownsAndDefaults: true,
+        },{
+          removeNonInheritableGroupAttrs: true,
+        },{
+          removeUselessStrokeAndFill: true,
+        },{
+          removeUnusedNS: true,
+        },{
+          cleanupIDs: true,
+        },{
+          cleanupNumericValues: true,
+        },{
+          moveElemsAttrsToGroup: true,
+        },{
+          moveGroupAttrsToElems: true,
+        },{
+          collapseGroups: true,
+        },{
+          removeRasterImages: false,
+        },{
+          mergePaths: true,
+        },{
+          convertShapeToPath: true,
+        },{
+          sortAttrs: false,
+        },{
+          transformsWithOnePath: false,
+        },{
+          removeDimensions: true,
+        },{
+          removeAttrs: {attrs: '(stroke|fill)'},
+        },{
+          addClassesToSVGElement: {
+                classNames: [svgClasses.value] // TODO warning if class is missing
+          }
+        }]
+      });
+
+    if (!inlineSVGExpression) {
+        return;
+    }
+
+    let builder = context.builder;
+
+    el.removeAttribute('inline-svg'); // This attribute is handled at compile time. We can just remove it now
+
+    if (inlineSVGExpression.type === 'Literal') {
+        let svgPath = "";
+
+        if(path.isAbsolute(inlineSVGExpression.value)){
+            const appDir = path.resolve(__dirname, '../..');
+            svgPath = path.normalize(appDir + inlineSVGExpression.value);
+        }else{
+            const templateDir = context.dirname;
+            svgPath = path.resolve(templateDir, inlineSVGExpression.value);
+        }
+
+        let data = fs.readFileSync(svgPath);
+        let optimisedSVG = svgoOptimizeSync(svgo, data.toString());
+        var svg = context.builder.htmlLiteral(optimisedSVG.data);
+
+        el.replaceWith(svg);
+    }
+};
