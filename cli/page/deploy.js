a880d5b26b1b63a5139b5099a43eaf074f97b916
Matej Šircelj <sircelj.m@gmail.com>
2019-01-12 12:18:16 +0100

    refactor cli/page/deploy.js

--- cli/page/deploy.js
+++ cli/page/deploy.js
@@ -1,6 +1,29 @@
 var path = require("path");
 
 module.exports = function(vorpal, projectConfig){
+
+    function deployPage( args, cb ){
+        const deployFn = args.options.edge ? require("../../lib/deploy-page-edge") : require("../../lib/deploy-page");
+        try{
+            projectConfig.credentials = require(projectConfig.credentials_path);
+        } catch(err){
+            if(err.code == "MODULE_NOT_FOUND"){
+                console.log("Credentials file at " + projectConfig.credentials_path + " could not be found. Exiting.");
+                console.log("Please change the \"credentials_path\" in " + path.resolve(projectConfig.cwd, "./project-config/main.config.js"));
+                console.log("Exiting.");
+                return;
+            }
+        }
+
+        deployFn(
+            projectConfig,
+            args.page,
+            args.options
+        );
+
+        // invokes command code in module providing vorpal and arguments, supporting promise as result
+        Promise.resolve( ( this, args ) ).then( projectConfig.repl ? cb : null );
+    }
     return vorpal
         .command("page-deploy <page>", "Deploy page to AWS" )
         .option("--no-gzip", "Do not compress lambda output")
@@ -12,35 +35,7 @@ module.exports = function(vorpal, projectConfig){
         .option("--lambda-name <name>", "Provide a custom name for your lambda")
         .option("--path <path>", "Provide a custom path for your lambda by default it equals lambda name")
         .alias("pd")
-        .action(( args, cb ) => {
-            try{
-                projectConfig.credentials = require(projectConfig.credentials_path);
-            } catch(err){
-                if(err.code == "MODULE_NOT_FOUND"){
-                    console.log("Credentials file at " + projectConfig.credentials_path + " could not be found. Exiting.");
-                    console.log("Please change the \"credentials_path\" in " + path.resolve(projectConfig.cwd, "./project-config/main.config.js"));
-                    console.log("Exiting.");
-                    return;
-                }
-            }
-
-            if(args.options.edge){
-                require("../../lib/deploy-page-edge")(
-                    projectConfig,
-                    args.page,
-                    args.options
-                );
-            }else{
-                require("../../lib/deploy-page")(
-                    projectConfig,
-                    args.page,
-                    args.options
-                );
-            }
-
-            // invokes command code in module providing vorpal and arguments, supporting promise as result
-            Promise.resolve( ( this, args ) ).then( projectConfig.repl ? cb : null );
-        })
+        .action(deployPage)
         .on("error", function(err){
             console.log(err);
         });
